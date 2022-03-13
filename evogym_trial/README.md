7dd3fb0c7accbf5c164d2047481a518edee34442
sttkm <st_tkm@websci.cs.tsukuba.ac.jp>
2022-03-13 23:19:27 +0900

    update trial

--- evogym_trial/README.md
+++ evogym_trial/README.md
@@ -9,6 +9,5 @@ $python run.py --env env_id
 #### options:
 - -t --task       : task id (default: Walker-v0)
 - --random        : sampling robot randomly
-- --height        : height of robot structure (default: 5)
-- --width         : width of robot structure (default: 5)
+- --shape         : robot shape (usage: "--shape {height} {width}", default: (5,5))
 - --deterministic : robot act deterministically in simulation
--- evogym_trial/run.py
+++ evogym_trial/run.py
@@ -17,14 +17,15 @@ def get_args():
         '-t', '--task', default='Walker-v0', help='evogym environment id')
     parser.add_argument(
         '--random', action='store_true', default=False, help='sample robot randomly')
-    parser.add_argument(
-        '--height', type=int, default=5, help="robot's height")
-    parser.add_argument(
-        '--width', type=int, default=5, help="robor's width")
+        parser.add_argument(
+        '--shape', default=[5,5], nargs='+', type=int, help='robot shape (useage: "--shape {height} {width}", default: (5,5))')
     parser.add_argument(
         '--deterministic', action='store_true', default=False, help='robot simulation deterministic')
 
     args = parser.parse_args()
+
+    assert len(args.shape)==2, 'argument error: use "--shape" option as "--shape {height} {width}"'
+
     return args
 
 
@@ -34,13 +35,12 @@ if __name__=='__main__':
 
     args = get_args()
 
-    robot_shape = (args.height, args.width)
 
     assert args.task in Tasks, \
         f'argumented task "{args.task}" is not prepared, so pick from ['+', '.join(Tasks)+'].'
 
     if args.random:
-        robot = sample_robot(robot_shape)
+        robot = sample_robot(args.shape)
     else:
         if args.task in Structures:
             structure = Structures[args.task]
@@ -48,7 +48,7 @@ if __name__=='__main__':
             robot = (structure, connections)
         else:
             print(f'robot on the task "{args.task}" is not defined, so sample robot randomly.')
-            robot = sample_robot(robot_shape)
+            robot = sample_robot(args.shape)
 
     print('robot structure: \n',robot[0].astype(int))
 
