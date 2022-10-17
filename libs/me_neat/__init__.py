8a5805eddb911cbcd26cd7ef17ee8162583f933d
sttkm <st_tkm@websci.cs.tsukuba.ac.jp>
2022-10-17 17:48:23 +0900

    fix class name

--- libs/me_neat/__init__.py
+++ libs/me_neat/__init__.py
@@ -1,6 +1,6 @@
 from neat_cppn import *
 from .population import Population
-from .behavioral_descriptor import LinerBehavioralDescriotir
+from .behavioral_descriptor import LinerBehavioralDescriptor
 from .reporting import BaseReporter, SaveResultReporter, MapElitesReporter
 from .drawer import BDDrawer
 from .config import make_config
